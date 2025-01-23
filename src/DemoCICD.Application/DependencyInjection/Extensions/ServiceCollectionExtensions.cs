1ed67ee7e33debcaa5b25d4dc17179385f796f34
duybao901 <duycarry123@gmail.com>
2025-01-23 21:33:24 +0700

    Add Performance log & Tracing log

--- src/DemoCICD.Application/DependencyInjection/Extensions/ServiceCollectionExtensions.cs
+++ src/DemoCICD.Application/DependencyInjection/Extensions/ServiceCollectionExtensions.cs
@@ -9,8 +9,10 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddConfigureMediatR(this IServiceCollection services)
      => services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(AssemblyReference.Assembly))
-        .AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationPipelineBehavior<,>))
-        .AddTransient(typeof(IPipelineBehavior<,>), typeof(TransactionPipelineBehavior<,>))
+        .AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationPipelineBehavior<,>)) // 1. Validate Request
+        .AddTransient(typeof(IPipelineBehavior<,>), typeof(TransactionPipelineBehavior<,>)) // 2. Global Transaction
+        .AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformancePipelineBehavior<,>)) // 3. Measure Permance
+        .AddTransient(typeof(IPipelineBehavior<,>), typeof(TracingPipelineBehavior<,>)) // 4. Log/Tracing Request
         .AddValidatorsFromAssembly(Contract.AssemblyReference.assembly, includeInternalTypes: true);
 
     public static IServiceCollection AddConfigureAutoMapper(this IServiceCollection services)
